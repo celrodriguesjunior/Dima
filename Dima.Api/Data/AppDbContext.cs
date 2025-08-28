4d33142c13d06134bc4940540b849233e0e806bf
Célio Rodrigues Junior <celio.junior@asv.com.br>
2025-08-28 08:11:34 -0300

    2025-08-28

--- Dima.Api/Data/AppDbContext.cs
+++ Dima.Api/Data/AppDbContext.cs
@@ -1,11 +1,22 @@
-﻿using Dima.Core.Models;
+﻿using Dima.Api.Models;
+using Dima.Core.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
 namespace Dima.Api.Data;
 
-public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext(options)
+public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<
+    User, //AspNetUsers
+    IdentityRole<long>, //AspNetRoles 
+    long, //AspNetUserRoles
+    IdentityUserClaim<long>,
+    IdentityUserRole<long>,
+    IdentityUserLogin<long>,
+    IdentityRoleClaim<long>,
+    IdentityUserToken<long>
+    >(options)
 {
     public DbSet<Category> Categories { get; set; }
     public DbSet<Transaction> Transactions { get; set; }
@@ -15,5 +26,7 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbCo
         base.OnModelCreating(modelBuilder);
         modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
     }
+
+
 
 }
