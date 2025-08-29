728e990dd9371188f5f9dff955be69cc57cbed5d
Célio Rodrigues Junior <celio.junior@asv.com.br>
2025-08-29 08:10:27 -0300

    2025-08-29

---
+++ Dima.Api/Data/Mappings/Identity/IdentityUserTokenMapping.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Dima.Api.Data.Mappings.Identity;
+
+public class IdentityUserTokenMapping : IEntityTypeConfiguration<IdentityUserToken<long>>
+{
+    public void Configure(EntityTypeBuilder<IdentityUserToken<long>> builder)
+    {
+        builder.ToTable("IdentityUserToken");
+        builder.HasKey(ut => new { ut.UserId, ut.LoginProvider, ut.Name });
+        builder.Property(x => x.LoginProvider).HasMaxLength(128);
+        builder.Property(x => x.Name).HasMaxLength(128);
+    }
+}
