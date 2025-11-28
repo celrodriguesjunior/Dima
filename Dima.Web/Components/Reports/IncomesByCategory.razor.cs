074febd75de762dcd667e8459009da1d7eb25d27
Célio Rodrigues Junior <55858108+celrodriguesjunior@users.noreply.github.com>
2025-11-28 08:00:06 -0300

    2025-11-28

---
+++ Dima.Web/Components/Reports/IncomesByCategory.razor.cs
@@ -0,0 +1,52 @@
+﻿using Dima.Core.Handlers;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace Dima.Web.Components.Reports;
+
+public partial class IncomesByCategoryComponentBase : ComponentBase
+{
+
+    #region Properties
+
+    public List<double> Data { get; set; } = [];
+    public List<string> Labels { get; set; } = [];
+
+    #endregion
+
+    #region Services
+
+    [Inject]
+    public IReportHandler Handler { get; set; } = null!;
+    [Inject]
+    public ISnackbar Snackbar { get; set; } = null!;
+
+    #endregion
+
+    #region Overrides   
+
+    protected override async Task OnInitializedAsync()
+    {
+        await GetIncomesByCategoryAsync();
+    }
+
+    private async Task GetIncomesByCategoryAsync()
+    {
+        var response = await Handler.GetIncomesByCategoryReport(new());
+        if (!response.IsSuccess || response.Data == null)
+        {
+            Snackbar.Add("Não foi possível carregar o gráfico de despesas por categoria.", Severity.Error);
+            return;
+        }
+
+        foreach (var item in response.Data)
+        {
+            Labels.Add($"{item.Category} ({item.Incomes:C})");
+            Data.Add(-(double)item.Incomes);//gráfico só espera valor positivo, por isso precisa inverter o valor
+        }
+
+    }
+
+    #endregion
+
+}
--- Dima.Web/obj/Debug/net9.0/Dima.Web.AssemblyInfo.cs
+++ Dima.Web/obj/Debug/net9.0/Dima.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dima.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28ecd039bbdad724abcf8b7b7e40b4b15fa6007f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e701f1a81e4ff71002036908c58ebe3f34724d86")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dima.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dima.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
